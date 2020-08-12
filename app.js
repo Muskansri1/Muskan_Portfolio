4056b2edc4b665e93fd61ce7db677b7145888fd2
Muskan <muskansri1@gmail.com>
2020-08-12 18:13:37 +0530

    finally ready for heroku

---
+++ app.js
@@ -0,0 +1,52 @@
+var express = require("express"),
+	app = express(),
+	bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/styles"));
+
+
+//Routes
+
+app.get("/", function(req, res){
+	res.render("front");
+});
+
+app.get("/home", function(req,res){
+	res.render("index");
+});
+
+app.get("/education", function(req,res){
+	res.render("education");
+});
+
+app.get("/skills", function(req,res){
+	res.render("skills");
+});
+
+app.get("/aboutme", function(req,res){
+	res.render("aboutme");
+});
+
+app.get("/blogs", function(req,res){
+	res.render("blogs");
+});
+
+app.get("/projects", function(req,res){
+	res.render("projects");
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+console.log('Server listening on port 3000  ');
+});
